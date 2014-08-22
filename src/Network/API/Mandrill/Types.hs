1c23e1a5cd9622521486c58e4a66065c5a85a820
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2014-08-22 09:25:47 +0200

    Code won't build until new release on base64

--- src/Network/API/Mandrill/Types.hs
+++ src/Network/API/Mandrill/Types.hs
@@ -10,6 +10,10 @@ import           Data.Char
 import           Data.Time
 import           Control.Applicative
 import           System.Locale (defaultTimeLocale)
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Base64 as Base64
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import           Control.Lens
@@ -124,6 +128,61 @@ instance Arbitrary MandrillHtml where
   arbitrary = pure $ mkMandrillHtml "<p><b>FooBar</b></p>"
 
 --------------------------------------------------------------------------------
+type MandrillTags = T.Text
+
+
+--------------------------------------------------------------------------------
+type MandrillHeader = Map T.Text T.Text
+
+
+--------------------------------------------------------------------------------
+type MandrillVars = Map T.Text T.Text
+
+
+--------------------------------------------------------------------------------
+data MandrillMergeVars = MandrillMergeVars {
+    _mmvr_rcpt :: !T.Text
+  , _mmvr_vars :: [MandrillVars]
+  } deriving Show
+
+makeLenses ''MandrillMergeVars
+deriveJSON defaultOptions { fieldLabelModifier = drop 6 } ''MandrillMergeVars
+
+--------------------------------------------------------------------------------
+data MandrillMetadata = MandrillMetadata {
+    _mmdt_rcpt :: !T.Text
+  , _mmdt_values :: [MandrillVars]
+  } deriving Show
+
+makeLenses ''MandrillMetadata
+deriveJSON defaultOptions { fieldLabelModifier = drop 6 } ''MandrillMetadata
+
+
+newtype Base64ByteString = B64BS !B.ByteString
+
+instance ToJSON Base64ByteString where
+  toJSON (B64BS bs) = Base64.encode bs
+
+instance FromJSON Base64ByteString where
+  parseJSON (String v) = case Base64.decode v of
+    Left err = fail err
+    Right rs = B64BS rs
+  parseJSON rest = typeMismatch "Base64ByteString must be a String." rest
+
+--------------------------------------------------------------------------------
+data MandrillWebContent = MandrillWebContent {
+    _mwct_type :: !T.Text
+  , _mwct_name :: !T.Text
+    -- ^ [for images] the Content ID of the image
+    -- - use <img src="cid:THIS_VALUE"> to reference the image
+    -- in your HTML content
+  , _mwct_content :: !Base64ByteString
+  } deriving Show
+
+makeLenses ''MandrillWebContent
+deriveJSON defaultOptions { fieldLabelModifier = drop 6 } ''MandrillWebContent
+
+--------------------------------------------------------------------------------
 -- | The information on the message to send
 data MandrillMessage = MandrillMessage {
    _mmsg_html :: MandrillHtml
@@ -138,6 +197,74 @@ data MandrillMessage = MandrillMessage {
    -- ^ Optional from name to be used
  , _mmsg_to :: [MandrillRecipient]
    -- ^ A list of recipient information
+ , _mmsg_headers :: [MandrillHeader]
+   -- ^ optional extra headers to add to the message (most headers are allowed)
+ , _mmsg_important :: Maybe Bool
+   -- ^ whether or not this message is important, and should be delivered ahead
+   -- of non-important messages
+ , _mmsg_track_opens :: Maybe Bool
+   -- ^ whether or not to turn on open tracking for the message
+ , _mmsg_track_clicks :: Maybe Bool
+   -- ^ whether or not to turn on click tracking for the message
+ , _mmsg_auto_text :: Maybe Bool
+   -- ^ whether or not to automatically generate a text part for messages that are not given text
+ , _mmsg_auto_html :: Maybe Bool
+   -- ^ whether or not to automatically generate an HTML part for messages that are not given HTML
+ , _mmsg_inline_css :: Maybe Bool
+   -- ^ whether or not to automatically inline all CSS styles provided in the message HTML
+   -- - only for HTML documents less than 256KB in size
+ , _mmsg_url_strip_qs :: Maybe Bool
+   -- ^ whether or not to strip the query string from URLs when aggregating tracked URL data
+ , _mmsg_preserve_recipients :: Maybe Bool
+   -- ^ whether or not to expose all recipients in to "To" header for each email
+ , _mmsg_view_content_link :: Maybe Bool
+   -- ^ set to false to remove content logging for sensitive emails
+ , _mmsg_bcc_address :: Maybe T.Text
+   -- ^ an optional address to receive an exact copy of each recipient's email
+ , _mmsg_tracking_domain :: Maybe T.Text
+   -- ^ a custom domain to use for tracking opens and clicks instead of mandrillapp.com
+ , _mmsg_signing_domain :: Maybe Bool
+   -- ^ a custom domain to use for SPF/DKIM signing instead of mandrill 
+   -- (for "via" or "on behalf of" in email clients)
+ , _mmsg_return_path_domain :: Maybe Bool
+   -- ^ a custom domain to use for the messages's return-path
+ , _mmsg_merge :: Maybe Bool
+   -- ^ whether to evaluate merge tags in the message.
+   -- Will automatically be set to true if either merge_vars
+   -- or global_merge_vars are provided.
+ , _mmsg_global_merge_vars :: [MandrillVars]
+   -- ^ global merge variables to use for all recipients. You can override these per recipient.
+ , _mmsg_merge_vars :: [MandrillMergeVars]
+   -- ^ per-recipient merge variables, which override global merge variables with the same name.
+ , _mmsg_tags :: [MandrillTags]
+   -- ^ an array of string to tag the message with. Stats are accumulated using tags,
+   -- though we only store the first 100 we see, so this should not be unique
+   -- or change frequently. Tags should be 50 characters or less.
+   -- Any tags starting with an underscore are reserved for internal use
+   -- and will cause errors.
+ , _mmsg_subaccount :: Maybe T.Text
+   -- ^ the unique id of a subaccount for this message
+   -- - must already exist or will fail with an error
+ , _mmsg_google_analytics_domains :: [T.Text]
+   -- ^ an array of strings indicating for which any matching URLs
+   -- will automatically have Google Analytics parameters appended
+   -- to their query string automatically.
+ , _mmsg_google_analytics_campaign :: Maybe T.Text
+   -- ^ optional string indicating the value to set for the utm_campaign
+   -- tracking parameter. If this isn't provided the email's from address
+   -- will be used instead.
+ , _mmsg_metadata :: [MandrillVars]
+   -- ^ metadata an associative array of user metadata. Mandrill will store
+   -- this metadata and make it available for retrieval.
+   -- In addition, you can select up to 10 metadata fields to index
+   -- and make searchable using the Mandrill search api.
+ , _mmsg_recipient_metadata :: [MandrillMetadata]
+   -- ^ Per-recipient metadata that will override the global values
+   -- specified in the metadata parameter.
+ , _mmsg_attachments :: [MandrillWebContent]
+   -- ^ an array of supported attachments to add to the message
+ , _mmsg_images :: [MandrillWebContent]
+   -- ^ an array of embedded images to add to the message
  } deriving Show
 
 makeLenses ''MandrillMessage
@@ -150,6 +277,31 @@ instance Arbitrary MandrillMessage where
                               <*> pure "sender@example.com"
                               <*> pure Nothing
                               <*> resize 2 arbitrary
+                              <*> pure []
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure Nothing
+                              <*> pure []
+                              <*> pure []
+                              <*> pure []
+                              <*> pure Nothing
+                              <*> pure []
+                              <*> pure Nothing
+                              <*> pure []
+                              <*> pure []
+                              <*> pure []
+                              <*> pure []
 
 --------------------------------------------------------------------------------
 type MandrillKey = T.Text
